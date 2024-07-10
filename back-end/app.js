5eb4a1a8f80cf4d43883f592118bae5b2bd3ecee
ameant <amelynea91@gmail.com>
2024-07-10 13:19:16 +0200

    test two

--- back-end/app.js
+++ back-end/app.js
@@ -31,7 +31,7 @@ const options = {
     },
     servers: [
       {
-        url: 'http://localhost:3000',
+        url: ['http://localhost:3000', 'https://port-de-plaisance-russell-api.onrender.com'],
 
       },
     ],
